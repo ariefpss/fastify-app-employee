3b6579d19adbe6c6cb9fc095ffb16b0b317ba82e
ariefpss <permanaarief8@gmail.com>
2021-11-06 21:19:00 +0900

    Create feature Pagination and Sorting (Follow for plugin smpSortableTable)

---
+++ public/js/pagination.js
@@ -0,0 +1,240 @@
+$(function(){
+    $.fn.pagination = function(data, limit) {
+
+        //Todo: Render/create table
+        function renderTable(start, end, limit, data) {
+            let tmpHTML = '';
+            for (let i = start; i < Math.min(end, limit); i++) {
+                tmpHTML +=  "<tr><td class='text-center'>"+data.users[i].id+
+                            "</td><td>"+data.users[i].name+
+                            "</td><td>"+data.users[i].email+
+                            "</td><td class='text-end'>"+data.users[i].mobile+
+                            "</td><td class='text-center'><a class='btn btn-success'>"+"Detail"+
+                                    "</a><a class='btn btn-success ms-3'>"+"Edit"+
+                                    "</a><a class='btn btn-danger ms-3'>"+"Delete"+
+                            "</a></td></tr>";
+            };
+
+            return tmpHTML;
+        };
+
+        //TODO: First Running pagination
+        let $table = $(this);
+        let tableId = $table.attr('id');
+        let page = 1;
+        limit = limit < 1 ? 5 : (limit || 5);
+
+        $table.find('tbody').empty().html(renderTable(0, data.users.length, limit, data));
+
+        $table.after(
+            "<div id='after' class='fixed-bottom'>"+
+                "<div class='container mb-4'>" +
+                    "<div class='justify-content-md-center row'>"+
+                        "<div class='col-md-auto'>"+
+                            "<label class='pt-1'>Show</label>"+
+                        "</div>"+
+                        "<div class='col-md-auto'>"+
+                            "<select class='form-select' id='limit'>"+
+                                "<option value=''></option>"+
+                                "<option value='5'>5</option>"+
+                                "<option value='10'>10</option>"+
+                                "<option value='15'>15</option>"+
+                            "</select>"+
+                        "</div>"+
+                        "<div class='col-md-auto'>"+
+                            "<label class='pt-1'>Per Page</label>"+
+                        "</div>"+
+                        "<div class='col-md-auto'>"+
+                            "<nav>"+
+                                "<ul class='pagination'>"+
+                                    "<li id='firstPrev' class='page-item'> <a class='page-link first-link'>&lt;</a> </li>"+
+                                    "<li id='previous' class='page-item'> <a class='page-link'>&laquo;</a> </li>"+
+                                    "<li id='page' class='page-item'></li>"+
+                                    "<li id='next' class='page-item'> <a class='page-link'>&raquo;</a> </li>"+
+                                    "<li id='lastNext' class='page-item'> <a class='page-link first-link'>&gt;</a> </li>"+
+                                "</ul>"+
+                            "</nav>"+
+                        "</div>"+
+                    "</div>"+
+                "</div>"+
+
+                "<div class='container-fluid bg-dark'>"+
+                    "<div class='row'>"+
+                        "<div class='col'>"+
+                            "<p class='text-center text-white my-2'>copyright© RiefDev 2021</p>"+
+                        "</div>"+
+                    "</div>"+
+                "</div>"+
+            "</div>"
+        );
+
+        //Todo: first counter pagination
+        if (data.users.length) {
+            let totalPage = Math.ceil(data.users.length/5);
+            let text = "<a class='page-link'>Page 1 of "+totalPage+"</a>";
+
+            $('ul.pagination > li#page').empty().append(text);
+            $('ul.pagination > li#previous').addClass('disabled');
+            $('ul.pagination > li#firstPrev').addClass('disabled');
+
+            counterPage(limit)
+
+        } else {
+            let text = "<a class='page-link'>Nothing Data</a>";
+
+            $('ul.pagination > li#page').empty().append(text);
+        };
+
+        counterPage(limit);
+
+        function counterPage(lim) {
+            if (data.users.length > lim) {
+                let totalPage = Math.ceil(data.users.length/lim);
+
+                $('ul.pagination > li#next').removeClass('disabled');
+                $('ul.pagination > li#lastNext').removeClass('disabled');
+
+                let text = "<a class='page-link'>Page "+page+" of "+totalPage+"</a>";
+                $('ul.pagination > li#page').empty().append(text);
+
+                $('ul.pagination > li#next').click(function(){
+                    if (!$(this).hasClass('disabled')) {
+                        let totalPage = Math.ceil(data.users.length/lim);
+                        let pagePlus = page + 1;
+
+                        let size = data.users.length
+                        let start = (pagePlus - 1) * lim;
+                        let end = pagePlus * lim;
+
+
+                        $table.find('tbody').empty().html(renderTable(start, size, end, data));
+
+                        let text = "<a class='page-link'>Page "+pagePlus+" of "+totalPage+"</a>";
+                        $('#page').empty().append(text);
+
+                        $('ul.pagination > li#firstPrev').removeClass('disabled');
+                        $('ul.pagination > li#previous').removeClass('disabled');
+                        if (end >= size) {
+                            $('ul.pagination > li#next').addClass('disabled');
+                            $('ul.pagination > li#lastNext').addClass('disabled');
+                        }
+                        page = pagePlus;
+                    };
+                });
+
+                $('ul.pagination > li#lastNext').click(function(){
+                    if (!$(this).hasClass('disabled')) {
+                        let totalPage = Math.ceil(data.users.length/lim);
+                        page = totalPage;
+
+                        let size = data.users.length;
+                        let start = (page - 1) * lim;
+                        let end = page * lim;
+
+                        $table.find('tbody').empty().html(renderTable(start, size, end, data));
+
+                        let text = "<a class='page-link'>Page "+page+" of "+totalPage+"</a>";
+                        $('ul.pagination > li#page').empty().append(text);
+
+                        $('ul.pagination > li#firstPrev').removeClass('disabled');
+                        $('ul.pagination > li#previous').removeClass('disabled');
+                        if (end >= size) {
+                            $('ul.pagination > li#next').addClass('disabled');
+                            $('ul.pagination > li#lastNext').addClass('disabled');
+                        };
+
+                    };
+                });
+
+                $('ul.pagination > li#previous').click(function(){
+                    if (!$(this).hasClass('disabled')) {
+                        let totalPage = Math.ceil(data.users.length/lim);
+                        let minPage = page - 1
+
+                        let size = data.users.length;
+                        let start = (minPage - 1) * lim;
+                        let end = minPage * lim;
+
+                        $table.find('tbody').empty().html(renderTable(start, size, end, data));
+
+                        let text = "<a class='page-link'>Page "+minPage+" of "+totalPage+"</a>";
+                        $('ul.pagination > li#page').empty().append(text);
+
+                        $('ul.pagination > li#next').removeClass('disabled');
+                        $('ul.pagination > li#lastNext').removeClass('disabled');
+                        if (!start) {
+                            $('ul.pagination > li#firstPrev').addClass('disabled');
+                            $('ul.pagination > li#previous').addClass('disabled');
+                        };
+
+                        page = minPage;
+                    };
+                });
+
+                $('ul.pagination > li#firstPrev').click(function(){
+                    if (!$(this).hasClass('disabled')) {
+                        let totalPage = Math.ceil(data.users.length/lim);
+                        page = 1;
+
+                        let size = data.users.length;
+                        let start = (page - 1) * lim;
+                        let end = page * lim;
+
+                        $table.find('tbody').empty().html(renderTable(start, size, end, data));
+
+                        let text = "<a class='page-link'>Page "+page+" of "+totalPage+"</a>";
+                        $('ul.pagination > li#page').empty().append(text);
+
+                        $('ul.pagination > li#next').removeClass('disabled');
+                        $('ul.pagination > li#lastNext').removeClass('disabled');
+                        if (!start) {
+                            $('ul.pagination > li#firstPrev').addClass('disabled');
+                            $('ul.pagination > li#previous').addClass('disabled');
+                        };
+                    };
+                });
+
+            } else if (data.users.length <= lim) {
+                $('ul.pagination > li#next').addClass('disabled');
+                $('ul.pagination > li#lastNext').addClass('disabled');
+                $('ul.pagination > li#previous').addClass('disabled');
+                $('ul.pagination > li#firstPrev').addClass('disabled');
+
+                let text = "<a class='page-link'>All Data</a>";
+                $('ul.pagination > li#page').empty().append(text);
+            };
+        };
+
+        $('.sorting').click(function () {
+
+            let direction = $(this).hasClass('asc') ? 'desc' : 'asc';
+            let columnName = $(this).attr('id');
+
+            $('.sorting').removeClass('asc desc');
+
+            data.users.sort(sortFn(columnName)[direction]);
+
+            $table.find('tbody').empty().html(renderTable(0, data.users.length, limit, data));
+
+            $(this).addClass(direction);
+        });
+
+        var sortFn = function (key) {
+            return {
+                desc: function (a, b) {
+                    if (typeof a[key] !== 'object')
+                        return a[key] > b[key] ? -1 : (a[key] < b[key] ? 1  : 0);
+                    else
+                        return a[key].sort > b[key].sort ? -1 : (a[key].sort < b[key].sort ? 1  : 0);
+                },
+                asc: function (a, b) {
+                    if (typeof a[key] !== 'object')
+                        return a[key] < b[key] ? -1 : (a[key] > b[key] ? 1  : 0);
+                    else
+                        return a[key].sort < b[key].sort ? -1 : (a[key].sort > b[key].sort ? 1  : 0);
+                }
+            };
+        };
+
+    };
+});
--- routes/Ruser.js
+++ routes/Ruser.js
@@ -2,12 +2,23 @@ const User = require('../config/database').user;
 
 async function routes (fastify, options) {
 
-    //TODO: Get view home
+    //TODO: Activity GET
+
+    //todo: get view home
     fastify.get('/', async (request, reply) => {
         User
             .findAll({limit: 5, offset: 0})
             .then((user) => {
                 reply.view('home', {title: 'Employee', users: user});
+            });
+    });
+
+    //todo: get all data in database
+    fastify.get('/user', async (request, reply) => {
+        User
+            .findAll()
+            .then((user) => {
+                reply.send({users: user});
             });
     });
 };
